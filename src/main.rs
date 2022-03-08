b96dd0f2e658a43998b28316f8833aa2de3402ff
bwallker <benbwall@gmail.com>
2022-03-08 13:34:31 +0200

    add tests and minor improvements

--- src/main.rs
+++ src/main.rs
@@ -7,6 +7,26 @@ use simple_logger::SimpleLogger;
 use std::env::{args};
 use std::hash::Hash;
 
+trait PopChar {
+  fn pop_char(&mut self) -> Option<char>;
+}
+
+
+impl PopChar for &str {
+  /// Pops the first char from the string and returns it. returns None if the string is empty.
+
+
+  fn pop_char(&mut self) -> Option<char> {
+    let top = self.chars().next();
+    match top {
+      None => None,
+      Some(v) => {
+        *self = &self[v.len_utf8()..];
+        Some(v)
+      }
+    }
+  }
+}
 
 trait OkOrDefault {
   type T;
@@ -58,10 +78,10 @@ fn string_parse_error_to_string(err: &StringParseErrorCode) -> String {
 enum ConfigParseErrorCode {
   ConfigNotFound,
   ConfigCouldNotBeCreated(String),
-  MissingEqualSign(u32),
-  MissingAliasValue(u32),
-  InvalidAlias(StringParseErrorCode, u32),
-  InvalidValue(StringParseErrorCode, u32),
+  MissingEqualSign(usize),
+  MissingAliasValue(usize),
+  InvalidAlias(StringParseErrorCode, usize),
+  InvalidValue(StringParseErrorCode, usize),
 }
 
 impl Display for ConfigParseErrorCode {
@@ -83,15 +103,16 @@ fn config_parse_error_to_string(err: &ConfigParseErrorCode) -> String {
 
 #[derive(Debug, Eq, PartialOrd, PartialEq, Ord, Hash, Clone)]
 enum CommandLineArgumentErrorCode {
-  MissingNameArgument(u32),
-  MissingContentArgument(u32),
-  FailedExecute(u32),
-  CannotRemoveNonExistentValue(String, u32),
-  InvalidAliasName(String, u32),
-  AliasDoesNotExist(String, u32),
-  AliasAlreadyExists(String, u32),
-  InvalidCommand(String, u32),
-  NoValidArgs(u32),
+  MissingNameArgument(usize),
+  MissingContentArgument(usize),
+  FailedExecute(String, usize),
+  CannotRemoveNonExistentValue(String, usize),
+  InvalidAliasName(String, usize),
+  AliasDoesNotExist(String, usize),
+  AliasAlreadyExists(String, usize),
+  InvalidCommand(String, usize),
+  NoValidArgs,
+  NoArgs,
 }
 
 use CommandLineArgumentErrorCode::*;
@@ -106,13 +127,14 @@ fn command_line_error_to_string(err: &CommandLineArgumentErrorCode) -> String {
   match err {
     MissingNameArgument(v) => format!("You did not supply a name argument for command. Error occurred at argument number {v}"),
     MissingContentArgument(v) => format!("You did not supply a content argument for command. Error occurred at argument number {v}"),
-    FailedExecute(v) => format!("Execution of execute command failed. Error occurred at argument number {v}"),
+    FailedExecute(error, v) => format!("Execution of execute command failed. Error occurred at argument number {v}. Here is the error the OS returned.{END_OF_LINE_SEQUENCE}{END_OF_LINE_SEQUENCE}{error}"),
     CannotRemoveNonExistentValue(name, v) => format!("Error removing alias of name {name}. This alias had no value associated with it and thus could not be removed. Error occurred at argument number {v}"),
     InvalidAliasName(name, v) => format!("Alias with name {name} has no content associated with it and thus cannot be executed. Error occurred at argument number {v}"),
     AliasDoesNotExist(name, v) => format!("Cannot change alias with name {name} because it does not exist. Error occurred at argument number {v}"),
     AliasAlreadyExists(name, v) => format!("Cannot create alias with name {name} that already exists. Error occurred at argument number {v}"),
     InvalidCommand(name, v) => format!("Command with name {name} does not exist. Error occurred at argument number {v}"),
-    NoValidArgs(v) => format!("You passed no valid arguments to Alia. Error occurred at argument number {v}"),
+    NoValidArgs => format!("You passed no valid arguments to Alia."),
+    NoArgs => format!("You did not pass any args to Alia."),
   }
 }
 
@@ -201,28 +223,31 @@ fn main() {
   }
 }
 
-type CommandLineArgParser<T> = &'static dyn Fn(&mut T, &mut u32, &mut HashMap<String, String>) -> Result<(), CommandLineArgumentErrorCode>;
+type CommandLineArgParser<T> = &'static dyn Fn(&mut T, &mut usize, &mut HashMap<String, String>) -> Result<(), CommandLineArgumentErrorCode>;
 
-fn get_next_arg<T: ExactSizeIterator<Item = String>>(args: &mut T, current_arg: &mut u32) -> Option<String> {
+fn get_next_arg<T: ExactSizeIterator<Item = String>>(args: &mut T, current_arg: &mut usize) -> Option<String> {
   let res = args.next()?;
   *current_arg += 1;
   Some(res)
 }
 
 fn parse_command_line_args<T: 'static + ExactSizeIterator<Item = String>>(mut args: T, cfg: &mut HashMap<String, String>) -> Result<(), CommandLineArgumentErrorCode> {
-  let mut current_arg: u32 = 0;
+  let mut current_arg: usize = 0;
+  if args.len() == 0 {
+    return Err(NoArgs);
+  }
   let maybe_path = args.next();
   let mut first_arg_invalid = false;
   if let Some(arg) = maybe_path {
-    current_arg += 1;
     let parser = parse_arg(arg.as_str()).unwrap_or_else(|| {
       first_arg_invalid = true;
       &do_nothing
     });
+    current_arg += 1;
     parser(&mut args, &mut current_arg, cfg)?;
   }
   if args.len() == 0 && first_arg_invalid {
-    return Err(NoValidArgs(current_arg));
+    return Err(NoValidArgs);
   }
   while let Some(arg) = args.next() {
     current_arg += 1;
@@ -239,11 +264,11 @@ fn parse_command_line_args<T: 'static + ExactSizeIterator<Item = String>>(mut ar
 
 fn parse_arg<T: ExactSizeIterator<Item = String>>(arg: &str) -> Option<CommandLineArgParser<T>> {
   let res: CommandLineArgParser<T> = match arg {
-    "-a" | "--add" => &add_alias,
-    "-r" | "--remove" => &remove_alias,
-    "-e" | "--execute" => &execute_alias,
-    "-c" | "--change" => &change_alias,
-    "-h" | "--help" => {
+    "a" | "add" => &add_alias,
+    "r" | "remove" => &remove_alias,
+    "e" | "execute" => &execute_alias,
+    "c" | "change" => &change_alias,
+    "h" | "help" => {
       display_help_message();
       &do_nothing
     }
@@ -252,20 +277,20 @@ fn parse_arg<T: ExactSizeIterator<Item = String>>(arg: &str) -> Option<CommandLi
   Some(res)
 }
 
-fn do_nothing<T: ExactSizeIterator<Item = String>>(_args: &mut T, _current_arg: &mut u32, _cfg: &mut HashMap<String, String>) -> Result<(), CommandLineArgumentErrorCode> { Ok(()) }
+fn do_nothing<T: ExactSizeIterator<Item = String>>(_args: &mut T, _current_arg: &mut usize, _cfg: &mut HashMap<String, String>) -> Result<(), CommandLineArgumentErrorCode> { Ok(()) }
 
-fn add_alias<T: ExactSizeIterator<Item = String>>(args: &mut T, current_arg: &mut u32, cfg: &mut HashMap<String, String>) -> Result<(), CommandLineArgumentErrorCode> {
+fn add_alias<T: ExactSizeIterator<Item = String>>(args: &mut T, current_arg: &mut usize, cfg: &mut HashMap<String, String>) -> Result<(), CommandLineArgumentErrorCode> {
   let name_of_alias = get_next_arg(args, current_arg).ok_or(MissingNameArgument(*current_arg))?;
-  let content_of_alias = get_next_arg(args, current_arg).ok_or(MissingContentArgument(*current_arg))?;
   let res = cfg.get(name_of_alias.as_str());
   if let Some(_) = res {
     return Err(AliasAlreadyExists(name_of_alias, *current_arg));
   }
+  let content_of_alias = get_next_arg(args, current_arg).ok_or(MissingContentArgument(*current_arg))?;
   cfg.insert(name_of_alias, content_of_alias);
   Ok(())
 }
 
-fn remove_alias<T: ExactSizeIterator<Item = String>>(args: &mut T, current_arg: &mut u32, cfg: &mut HashMap<String, String>) -> Result<(), CommandLineArgumentErrorCode> {
+fn remove_alias<T: ExactSizeIterator<Item = String>>(args: &mut T, current_arg: &mut usize, cfg: &mut HashMap<String, String>) -> Result<(), CommandLineArgumentErrorCode> {
   let name_of_alias = get_next_arg(args, current_arg).ok_or(MissingNameArgument(*current_arg))?;
   match cfg.remove(&name_of_alias) {
     Some(_) => Ok(()),
@@ -273,44 +298,34 @@ fn remove_alias<T: ExactSizeIterator<Item = String>>(args: &mut T, current_arg: 
   }
 }
 
-fn execute_alias<T: ExactSizeIterator<Item = String>>(args: &mut T, current_arg: &mut u32, cfg: &mut HashMap<String, String>) -> Result<(), CommandLineArgumentErrorCode> {
+fn execute_alias<T: ExactSizeIterator<Item = String>>(args: &mut T, current_arg: &mut usize, cfg: &mut HashMap<String, String>) -> Result<(), CommandLineArgumentErrorCode> {
   let name_of_alias = get_next_arg(args, current_arg).ok_or(MissingNameArgument(*current_arg))?;
   let content_of_alias = cfg.get(name_of_alias.as_str()).ok_or(InvalidAliasName(name_of_alias, *current_arg))?;
   let split = content_of_alias.split_whitespace();
-  let iter = [RUN_AS_COMMAND_IN_OS].iter().map(|x| *x);
+  let iter = [RUN_AS_COMMAND_IN_OS].into_iter();
   let iter = iter.chain(split);
   let output = Command::new(NAME_OF_TERMINAL_PROGRAM).args(iter).spawn();
   match output {
     Ok(_) => Ok(()),
-    Err(_) => Err(FailedExecute(*current_arg))
+    Err(e) => Err(FailedExecute(e.to_string(), *current_arg))
   }
 }
 
-fn change_alias<T: ExactSizeIterator<Item = String>>(args: &mut T, current_arg: &mut u32, cfg: &mut HashMap<String, String>) -> Result<(), CommandLineArgumentErrorCode> {
+fn change_alias<T: ExactSizeIterator<Item = String>>(args: &mut T, current_arg: &mut usize, cfg: &mut HashMap<String, String>) -> Result<(), CommandLineArgumentErrorCode> {
   let name_of_alias = get_next_arg(args, current_arg).ok_or(MissingNameArgument(*current_arg))?;
-  let content_of_alias = get_next_arg(args, current_arg).ok_or(MissingContentArgument(*current_arg))?;
-
-
   let res = cfg.get(name_of_alias.as_str());
   if let None = res {
     return Err(AliasDoesNotExist(name_of_alias, *current_arg));
   }
+  let content_of_alias = get_next_arg(args, current_arg).ok_or(MissingContentArgument(*current_arg))?;
   cfg.insert(name_of_alias, content_of_alias);
   Ok(())
 }
 
-fn read_from_config_file() -> Result<HashMap<String, String>, ConfigParseErrorCode> {
-  let contents = fs::read_to_string(PATH_TO_CONFIG);
-  if let Err(_) = contents {
-    if let Err(e) = File::create(PATH_TO_CONFIG) {
-      return Err(ConfigCouldNotBeCreated(e.to_string()));
-    }
-    return Err(ConfigNotFound);
-  }
-  let contents = unsafe { contents.unwrap_unchecked() };
-  let slice = &mut contents.trim();
-  let mut result = HashMap::with_capacity(contents.lines().count());
-  let mut current_alias: u32 = 1;
+fn parse_config(config_as_string: String) -> Result<HashMap<String, String>, ConfigParseErrorCode> {
+  let slice = &mut config_as_string.trim();
+  let mut result = HashMap::with_capacity(config_as_string.lines().count());
+  let mut current_alias: usize = 1;
   while slice.len() > 0 {
     let alias = parse_string(slice);
     if let Err(e) = alias {
@@ -318,14 +333,13 @@ fn read_from_config_file() -> Result<HashMap<String, String>, ConfigParseErrorCo
     }
     let alias = unsafe { alias.unwrap_unchecked() };
     *slice = slice.trim_start();
-    let next = slice.chars().next();
+    let next = slice.pop_char();
     if let None = next {
       return Err(MissingAliasValue(current_alias));
     }
     if next != Some('=') {
       return Err(MissingEqualSign(current_alias));
     }
-    *slice = &slice[1..];
     let value = parse_string(slice);
     if let Err(e) = value {
       return Err(InvalidValue(e, current_alias));
@@ -338,7 +352,18 @@ fn read_from_config_file() -> Result<HashMap<String, String>, ConfigParseErrorCo
   Ok(result)
 }
 
-fn write_to_config_file(cfg: &HashMap<String, String>) -> Result<(), ()> {
+fn read_from_config_file() -> Result<HashMap<String, String>, ConfigParseErrorCode> {
+  let contents = fs::read_to_string(PATH_TO_CONFIG);
+  if let Err(_) = contents {
+    if let Err(e) = File::create(PATH_TO_CONFIG) {
+      return Err(ConfigCouldNotBeCreated(e.to_string()));
+    }
+    return Err(ConfigNotFound);
+  }
+  parse_config(unsafe { contents.unwrap_unchecked() })
+}
+
+fn config_to_string(cfg: &HashMap<String, String>) -> String {
   let mut result_string = String::with_capacity(cfg.len() * 50);
   for thing in cfg {
     result_string.push('"');
@@ -348,6 +373,11 @@ fn write_to_config_file(cfg: &HashMap<String, String>) -> Result<(), ()> {
     result_string.push('"');
     result_string.push_str(END_OF_LINE_SEQUENCE);
   }
+  result_string
+}
+
+fn write_to_config_file(cfg: &HashMap<String, String>) -> Result<(), ()> {
+  let result_string = config_to_string(cfg);
   match fs::write("./cfg.alia", result_string) {
     Ok(()) => Ok(()),
     Err(_) => Err(()),
@@ -360,10 +390,9 @@ fn parse_string(slice: &mut &str) -> Result<String, StringParseErrorCode> {
 
 fn parse_string_track_lines(slice: &mut &str, current_line: &mut u32) -> Result<String, StringParseErrorCode> {
   *slice = slice.trim_start();
-  if slice.chars().next() != Some('"') {
+  if slice.pop_char() != Some('"') {
     return Err(StringWithoutOpeningQuote);
   }
-  *slice = &slice[1..];
   let ret_slice: &str = *slice;
   let current_char = slice.chars().next();
   if let None = current_char {
@@ -377,7 +406,7 @@ fn parse_string_track_lines(slice: &mut &str, current_line: &mut u32) -> Result<
       *current_line += 1;
     }
     if current_char == '"' && backslashes_in_a_row % 2 == 0 {
-      *slice = &slice[1..];
+      slice.pop_char();
 
       break;
     }
@@ -386,7 +415,7 @@ fn parse_string_track_lines(slice: &mut &str, current_line: &mut u32) -> Result<
     } else {
       backslashes_in_a_row = 0;
     }
-    *slice = &slice[1..];
+    slice.pop_char();
     let current_char_option = slice.chars().next();
     if let None = current_char_option {
       return Err(StringWithoutClosingQuote);
@@ -416,15 +445,15 @@ fn parse_string_track_lines(slice: &mut &str, current_line: &mut u32) -> Result<
 
 fn display_help_message() {
   printlnln!("Available commands are:");
-  println!("-a --add ---- Add an alias to Alia ---- Takes name of the alias and the content of the alias as arguments");
+  println!("a add ---- Add an alias to Alia ---- Takes name of the alias and the content of the alias as arguments");
   printlnln!("Example usage: alia --add run_release \"cargo run --release\"");
-  println!("-r --remove ---- Remove an alias from Alia ---- Takes the name of the alias to remove as an argument");
+  println!("r remove ---- Remove an alias from Alia ---- Takes the name of the alias to remove as an argument");
   printlnln!("Example usage: alia --remove run_release");
-  println!("-c --change ---- changes an alias in Alia ---- Takes the name of the alias and the new content as arguments");
+  println!("c change ---- changes an alias in Alia ---- Takes the name of the alias and the new content as arguments");
   printlnln!("Example usage: alia --change my_alias \"echo test\"");
-  println!("-e --execute ---- Executes the given alias ---- Takes the name of the alias to execute as an argument");
+  println!("e execute ---- Executes the given alias ---- Takes the name of the alias to execute as an argument");
   printlnln!("Example usage: alia --execute my_alias");
-  println!("-h --help ---- Displays this message");
+  println!("h help ---- Displays this message");
   printlnln!("Example usage: alia --help");
 }
 
@@ -433,8 +462,11 @@ fn display_help_message() {
 mod tests {
   #![allow(warnings)]
 
-  use crate::{parse_string, StringParseErrorCode};
+  use std::collections::HashMap;
+  use crate::{CommandLineArgumentErrorCode, config_to_string, ConfigParseErrorCode, END_OF_LINE_SEQUENCE, parse_command_line_args, parse_config, parse_string, StringParseErrorCode};
+  use CommandLineArgumentErrorCode::*;
   use StringParseErrorCode::*;
+  use ConfigParseErrorCode::*;
 
   #[test]
   fn test_parse_string() {
@@ -460,5 +492,70 @@ mod tests {
     let parsed = parse_string(slice);
     assert_eq!(parsed, expected_result);
     assert_eq!(slice, &expected_slice_at_end);
+  }
+
+  #[test]
+  fn test_parse_command_line_args() {
+    test_cmd_args_no_cfg(["help"], Ok(()));
+    test_cmd_args_no_cfg([""], Err(NoValidArgs));
+    test_cmd_args_no_cfg([], Err(NoArgs));
+    test_cmd_args_no_cfg(["h", "add", "my_alias", "echo test"], Ok(()));
+    test_cmd_args_template(["a", "my_alias", "echo test"], [], Ok(()), [("my_alias", "echo test")]);
+    test_cmd_args_template(["a", "my_alias", "echo test", "r", "my_alias"], [], Ok(()), []);
+    test_cmd_args_template(["c", "my_alias", "echo test"], [], Err(AliasDoesNotExist("my_alias".to_string(), 2)), []);
+    test_cmd_args_template(["e", "my_alias"], [], Err(InvalidAliasName("my_alias".to_string(), 2)), []);
+    test_cmd_args_template(["a", "my_alias", "echo test", "e", "my_alias"], [], Ok(()), [("my_alias", "echo test")]);
+  }
+
+  fn test_cmd_args_no_cfg<const NumOfArgs: usize>(args: [&'static str; NumOfArgs], expected_result: Result<(), CommandLineArgumentErrorCode>) {
+    println!("Entered test_cmd_args_no_cfg with args : {args:?}");
+    let res = parse_command_line_args(args.into_iter().map(|x| x.to_string()), &mut HashMap::new());
+    assert_eq!(res, expected_result);
+  }
+
+  fn test_cmd_args_template<const NumOfArgs: usize, const SizeOfCfg: usize, const SizeOfCfgAfter: usize>(args: [&'static str; NumOfArgs], cfg: [(&'static str, &'static str); SizeOfCfg], expected_result: Result<(), CommandLineArgumentErrorCode>, cfg_after: [(&'static str, &'static str); SizeOfCfgAfter]) {
+    let mut cfg: HashMap<String, String> = cfg.into_iter().map(|x| (x.0.to_string(), x.1.to_string())).collect();
+    let cfg_after: HashMap<String, String> = cfg_after.into_iter().map(|x| (x.0.to_string(), x.1.to_string())).collect();
+    println!("Entered test_cmd_args_template with args : {args:?}");
+    let res = parse_command_line_args(args.into_iter().map(|x| x.to_string()), &mut cfg);
+    assert_eq!(res, expected_result);
+    assert_eq!(cfg, cfg_after);
+  }
+
+  #[test]
+  fn test_parse_cfg() {
+    test_parse_cfg_template(["\"my_alias\" = \"echo test\"", "\"test\" = \"echo benis\""], Ok([("my_alias", "echo test"), ("test", "echo benis")]));
+    test_parse_cfg_template::<1, 1>(["my_alias = echo test"], Err(InvalidAlias(StringWithoutOpeningQuote, 1)));
+    test_parse_cfg_template::<1, 1>(["\"my_alias = echo test\""], Err(MissingAliasValue(1)));
+    test_parse_cfg_template::<1, 1>(["\"my_alias\"\"ttt\""], Err(MissingEqualSign(1)));
+    test_parse_cfg_template(["\"my_alias\"=\"t\"\"2\"=\"1\""], Ok([("my_alias", "t"), ("2", "1")]));
+  }
+
+  fn test_parse_cfg_template<const SizeOfCfg: usize, const SizeOfParsedCfg: usize>(cfg: [&'static str; SizeOfCfg], expected_result: Result<[(&'static str, &'static str); SizeOfParsedCfg], ConfigParseErrorCode>) {
+    let cfg = {
+      let mut temp = String::with_capacity(cfg.len() * 50);
+      for x in cfg {
+        temp.push_str(x);
+        temp.push_str(END_OF_LINE_SEQUENCE);
+      }
+      temp
+    };
+    let res = parse_config(cfg);
+    let expected_result = expected_result.map(|x| HashMap::from(x.map(|y| (y.0.to_string(), y.1.to_string()))));
+    assert_eq!(res, expected_result);
+  }
+
+  #[test]
+  fn test_cfg_to_string() {
+    test_cfg_to_string_template([("my_alias", "echo test")], [format!("\"my_alias\" = \"echo test\"{END_OF_LINE_SEQUENCE}")]);
+    test_cfg_to_string_template([("my_alias", "echo test"), ("1", "2")], [format!("\"my_alias\" = \"echo test\"{END_OF_LINE_SEQUENCE}\"1\" = \"2\"{END_OF_LINE_SEQUENCE}"), format!("\"1\" = \"2\"{END_OF_LINE_SEQUENCE}\"my_alias\" = \"echo test\"{END_OF_LINE_SEQUENCE}")]);
+  }
+
+  fn test_cfg_to_string_template<const SizeOfCfg: usize, const NumOfAcceptableResults: usize>(cfg: [(&'static str, &'static str); SizeOfCfg], expected_result: [String; NumOfAcceptableResults]) {
+    let res = config_to_string(&HashMap::from(cfg.map(|x| (x.0.into(), x.1.into()))));
+    if expected_result.iter().any(|x| x == &res) {
+      return;
+    }
+    panic!("result {res} did not match with any acceptable result. acceptable results are {expected_result:?}");
   }
 }
